0732ec58ca22f652cfe74434087ad42f75004dd0
Leow Chee Siang <cheesiangleow5284@gmail.com>
2019-01-17 18:31:44 +0900

    init

---
+++ client_camera.py
@@ -0,0 +1,33 @@
+import cv2
+import numpy as np
+import socket
+import sys
+import pickle
+import struct 
+import time
+
+cap=cv2.VideoCapture(0)
+clientsocket=socket.socket(socket.AF_INET,socket.SOCK_STREAM)
+clientsocket.settimeout(10)
+clientsocket.connect(('192.168.1.88',8089))
+
+while True:
+    try:
+        ret,frame=cap.read()
+        data = pickle.dumps(frame)
+        clientsocket.sendall(struct.pack("L", len(data))+data)
+        time.sleep(0.5)
+        if (cv2.waitKey(1) & 0xFF == ord('q')):
+            time.sleep(1)
+            print("disconnecting...")
+            clientsocket.close()
+            break
+    except KeyboardInterrupt:
+            print('Interrupted')
+            clientsocket.close()
+            cap.release()
+            sys.exit()
+
+clientsocket.close()
+cap.release()
+print("done")
---
+++ server_camera.py
@@ -0,0 +1,53 @@
+import socket
+import sys
+import cv2
+import pickle
+import numpy as np
+import struct ## new
+import base64
+
+HOST='192.168.1.80'
+PORT=8089
+
+s=socket.socket(socket.AF_INET,socket.SOCK_STREAM)
+print('Socket created')
+
+s.bind((HOST,PORT))
+print('Socket bind complete')
+s.listen(10)
+print('Socket now listening')
+
+conn,addr=s.accept()
+
+### new
+data = b''
+payload_size = struct.calcsize("L")
+while True:
+    try:
+        while len(data) < payload_size:
+            data += conn.recv(4096*2)
+            if not data:
+                break
+        if len(data) == 0 or len(data) < payload_size:
+            cv2.destroyAllWindows()
+            pass
+        else:
+            packed_msg_size = data[:payload_size]
+            data = data[payload_size:]
+            msg_size = struct.unpack("L", packed_msg_size)[0]
+            while len(data) < msg_size:
+                data += conn.recv(4096*2)
+            frame_data = data[:msg_size]
+            data = data[msg_size:]
+            print(data)
+            ###
+            frame=pickle.loads(frame_data)
+            # print(frame)
+            cv2.imshow('frame',frame)
+            cv2.waitKey(1)
+    except socket.error:
+        time.wait(1)
+        cv2.destroyAllWindows()
+
+    
+    
